90b7dd7b8d7414e48ca86675a4a5ccfda77f36ca
matheusgenê <76538896+matheusgene@users.noreply.github.com>
2021-07-17 22:40:24 -0300

    todos os estilos do site (CSS)

---
+++ src/components/champ-list/styles.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-left: 38px;
+`;
+
+export const Input = styled.input`
+  width: 220px;
+  height: 52px;
+  background-color: black;
+  border: none;
+  box-shadow: none;
+  margin-right: 1px;
+  color: #eee;
+`;
+
+export const A = styled.a`
+  letter-spacing: 0.08em;
+  font-style: italic;
+  font-weight: 800;
+  color: #ffffff;
+  text-decoration: none;
+`;
+
+export const Name = styled.div`
+  display: flex;
+  justify-content: center;
+  justify-content: space-around;
+  align-items: center;
+  height: 50px;
+  font-size: 20px;
+  font-weight: 800;
+  background-color: black;
+  margin: -4px 0px 20px 0px;
+`;
+
+export const Herois = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin: 20px 0px 0px 0px;
+  width: auto;
+`;
