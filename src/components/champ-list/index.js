f6062559e7e0d9db1a5140e6d9a7b81ea9759d13
matheusgenê <76538896+matheusgene@users.noreply.github.com>
2021-07-31 17:16:24 -0300

    coloquei o Select e o Input, setei os estados da api dentro do useEffect

--- src/components/champ-list/index.js
+++ src/components/champ-list/index.js
@@ -1,38 +1,60 @@
 import { useEffect, useState } from "react";
-import { Herois, Name, A, Wrapper, Input } from "./styles";
+import { Herois, Wrapper, Input, Select, A, Name } from "./styles";
+
 export function App() {
-  const [champs, setChamps] = useState("");
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState(""); // salva o valor digitado pelo usuario(nome)
+  const [select, setSelect] = useState(""); // salva o valor escolhido no select pelo usuario(classes)
+
+  const [fullList, setFullList] = useState([]);
+  const [filterSelect, setFilterSelect] = useState([]);
+  const [champs, setChamps] = useState([]);
 
   useEffect(() => {
     const fetchChampsLol = async () => {
       const response = await fetch(
         "https://ddragon.leagueoflegends.com/cdn/11.14.1/data/pt_BR/champion.json"
       );
-      const { data } = await response.json();
-      setChamps(data);
+      const result = await response.json();
+      setFilterSelect(Object.values(result.data));
+      setFullList(Object.values(result.data));
+      setChamps(Object.values(result.data));
     };
     fetchChampsLol();
   }, []);
 
-  const sos = Object.values(champs).map((n) => n.id);
-  const sos2 = sos.filter((c) => c.includes(input));
-
-  const lol = sos2.map((n, index) => (
-    <A
-      href={`https://br.leagueoflegends.com/pt-br/champions/${n.toLowerCase()}`}
-    >
-      <img
-        src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${n}_0.jpg`}
-        alt="champions"
-        key={index}
-      ></img>
-      <Name>{n}</Name>
-    </A>
-  ));
+  useEffect(() => {
+    console.log("render");
+    console.log(input);
+  }, [input]);
 
   function handleChange(e) {
-    setInput(e.target.value.charAt(0).toUpperCase() + e.target.value.substr(1));
+    const valorInput = e.target.value;
+    setInput(valorInput); //seta o input como (a)
+    // filterSelect = []
+    const filteredChamps = filterSelect.filter((c) =>
+      c.id.toUpperCase().includes(valorInput.toUpperCase())
+    );
+    setChamps(filteredChamps); //setou os campeões com (ahri) na tela
+  }
+
+  function selectChange(e) {
+    const valorSelect = e.target.value;
+    setSelect(valorSelect); //seta "" no select
+
+    let filteredClasses = fullList;
+
+    if (valorSelect !== "") {
+      filteredClasses = fullList.filter((c) => c.tags.includes(valorSelect));
+    }
+    //Champeõs Suport ex: todos
+    //valorInput = (ahri)
+    // 1 valor = NAMI.includes(AHRI)
+    const filteredInput = filteredClasses.filter((c) =>
+      c.id.toUpperCase().includes(input.toUpperCase())
+    );
+    //filterdInput = []
+    setChamps(filteredInput);
+    setFilterSelect(filteredClasses);
   }
 
   return (
@@ -44,8 +66,31 @@ export function App() {
           value={input}
           onChange={handleChange}
         />
+        <Select value={select} onChange={selectChange}>
+          <option value={""}>Todos</option>
+          <option value={"Fighter"}> Lutador </option>
+          <option value={"Mage"}> Mago </option>
+          <option value={"Assassin"}> Assasino </option>
+          <option value={"Marksman"}>atirador</option>
+          <option value={"Support"}> Suporte </option>
+          <option value={"Tank"}> Tanke </option>
+        </Select>
       </Wrapper>
-      <Herois>{lol}</Herois>
+
+      <Herois>
+        {champs.map((c) => (
+          <A
+            key={c.id}
+            href={`https://br.leagueoflegends.com/pt-br/champions/${c.id.toLowerCase()}`}
+          >
+            <img
+              src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${c.id}_0.jpg`}
+              alt="champions"
+            ></img>
+            <Name>{c.id}</Name>
+          </A>
+        ))}
+      </Herois>
     </div>
   );
 }
